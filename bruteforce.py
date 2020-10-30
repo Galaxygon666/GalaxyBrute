e97c45b7e3c6b453a7c0bc6d3ef97e8a70612b4c
Galaxygon666 <thepower2311@gmail.com>
2020-10-30 16:28:49 +0100

    Removed comments

--- bruteforce.py
+++ bruteforce.py
@@ -7,7 +7,7 @@ def main():
     input_pass = input("Insert md5 encrypted password: \n")
     with open("wordlist.txt", encoding="utf-8") as f:
         content = f.readlines()
-    # you may also want to remove whitespace characters like `\n` at the end of each line
+
     content = [x.strip() for x in content]
 
     start_time = time.time()
@@ -20,7 +20,7 @@ def cracker(content, input):
     for ele in content:
         temp_pass = hashlib.md5(ele.encode())
         pass_guess = str(temp_pass.hexdigest())
-        #print(pass_guess)
+
         if str(pass_guess) == str(input):
             return ele
             break
