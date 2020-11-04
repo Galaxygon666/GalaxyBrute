74a339dd9679114ba507cf98eb91f938c71fe552
Galaxygon666 <ngaming231@gmail.com>
2020-11-04 15:11:56 +0100

    Custom wordlist support added

--- bruteforce.py
+++ bruteforce.py
@@ -5,25 +5,35 @@ import pyfiglet
 def main():
     print(pyfiglet.figlet_format("GalaxyBrute"))
     input_pass = input("Insert md5 encrypted password: \n")
-    with open("wordlist.txt", encoding="utf-8") as f:
+    input_wordlist = input("Input path to wordlist: \n")
+    with open(input_wordlist, "r", encoding="ISO-8859-1") as f:
         content = f.readlines()
 
-    content = [x.strip() for x in content]
+    wordlist_list = []
+
+    for line in content:
+        wordlist_list.append(line.strip())
+
 
     start_time = time.time()
     print("\n\n" + "-"*14 + "GalaxyBrute" + "-"*14 + "\n")
-    print("Password: " + cracker(content, input_pass) + "\n")
+    cracker(wordlist_list, input_pass)
     print("--- in %s seconds ---" % (time.time() - start_time))
 
-
-def cracker(content, input):
-    for ele in content:
+def cracker(wordlist, input):
+    for ele in wordlist:
+        print("Trying: " + ele)
         temp_pass = hashlib.md5(ele.encode())
         pass_guess = str(temp_pass.hexdigest())
 
         if str(pass_guess) == str(input):
-            return ele
+            print("\n\n" + "-"*14 + "Password found!" + "-"*14 + "\n")
+            print("Password: " + ele + "\n")
+            print("-" * 43 + "\n")
+            return 0
             break
+
+    return "Password not found :("
 
 if __name__ == '__main__':
     main()
