c30ec4a83ab718e788e21bebc3209daf9322696a
Galaxygon666 <ngaming231@gmail.com>
2020-11-04 22:28:39 +0100

    Added sha-1 and sha-256 support

--- bruteforce.py
+++ bruteforce.py
@@ -1,29 +1,87 @@
+import sys
 import hashlib
 import time
 import pyfiglet
 
 def main():
     print(pyfiglet.figlet_format("GalaxyBrute"))
-    input_pass = input("Insert md5 encrypted password: \n")
-    input_wordlist = input("Input path to wordlist: \n")
-    with open(input_wordlist, "r", encoding="ISO-8859-1") as f:
+    input_method = input("Choose hashing algorithm:\n" +
+    "1. MD5\n" +
+    "2. SHA-1\n" +
+    "3. SHA-256\n" +
+    ">")
+
+    input_pass = input("Insert encrypted password: \n>")
+    input_wordlist = input("Input path to wordlist: \n>")
+    wordlister(input_wordlist, input_pass, int(input_method))
+    
+
+def wordlister(wordlist, password, method):
+    pass_test = []
+    with open(wordlist, "r", encoding="ISO-8859-1") as f:
         content = f.readlines()
 
-    wordlist_list = []
-
     for line in content:
-        wordlist_list.append(line.strip())
+        pass_test.append(line.strip())
 
 
-    start_time = time.time()
-    print("\n\n" + "-"*14 + "GalaxyBrute" + "-"*14 + "\n")
-    cracker(wordlist_list, input_pass)
-    print("------ %s seconds ------" % (time.time() - start_time))
+    if method == 1:
+        start_time = time.time()
+        print("\n\n" + "-"*14 + "GalaxyBrute" + "-"*14 + "\n")
+        md5(pass_test, password)
+        print("------ %s seconds ------" % (time.time() - start_time))
 
-def cracker(wordlist, input):
+    elif method == 2:
+        start_time = time.time()
+        print("\n\n" + "-"*14 + "GalaxyBrute" + "-"*14 + "\n")
+        sha1(pass_test, password)
+        print("------ %s seconds ------" % (time.time() - start_time))
+
+    elif method == 3:
+        start_time = time.time()
+        print("\n\n" + "-"*14 + "GalaxyBrute" + "-"*14 + "\n")
+        sha256(pass_test, password)
+        print("------ %s seconds ------" % (time.time() - start_time))
+
+    else:
+        print("Somn went wrong boss")
+
+
+
+def md5(wordlist, input):
     for ele in wordlist:
         print("Trying: " + ele)
         temp_pass = hashlib.md5(ele.encode())
+        pass_guess = str(temp_pass.hexdigest())
+
+        if str(pass_guess) == str(input):
+            print("\n\n" + "-"*14 + "Password found!" + "-"*14 + "\n")
+            print("Password: " + ele + "\n")
+            print("-" * 43 + "\n")
+            return 0
+            break
+
+    print("\n\n" + "-"*14 + "Password not found!" + "-"*14 + "\n")
+
+def sha1(wordlist, input):
+    for ele in wordlist:
+        print("Trying: " + ele)
+        temp_pass = hashlib.sha1(ele.encode())
+        pass_guess = str(temp_pass.hexdigest())
+
+        if str(pass_guess) == str(input):
+            print("\n\n" + "-"*14 + "Password found!" + "-"*14 + "\n")
+            print("Password: " + ele + "\n")
+            print("-" * 43 + "\n")
+            return 0
+            break
+
+    print("\n\n" + "-"*14 + "Password not found!" + "-"*14 + "\n")
+
+def sha256(wordlist, input):
+    for ele in wordlist:
+        print("Trying: " + ele)
+        temp_pass = hashlib.sha256(ele.encode())
         pass_guess = str(temp_pass.hexdigest())
 
         if str(pass_guess) == str(input):
